907ce993028dbd2d56c1d252dd3b105e70235605
DanieleMarino70 <danielaotitan@gmail.com>
2023-01-26 16:39:46 +0100

    add: dices User vs Cpu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 // TRACCIA:
 
+/*
+    Mail:
+
+    Chiedi all'utente la sua email,
+    controlla che sia nella lista di chi può accedere,
+    stampa un messaggio appropriato sull'esito del controllo.
+*/
+
+/* 
+    Gioco dei dadi:
+
+    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+
+
+// GENERO NUMERI RANDOM DA INSERIRE IN DELLE VARIABILI
+// DOPODICHE LI CONFRONTO CON I RISULTATI 
+//     - IL PRIMO E' MAGGIORE DEL SECONDO
+//     - IL PRIMO E' MINORE DEL SECONDO
+//     - IL PRIMO E' UGUALE AL SECONDO
+
+const randomUser = Math.floor(Math.random() * 6 + 1); 
+const randomCpu = Math.floor(Math.random() * 6 + 1); 
+
+if(randomUser > randomCpu) {
+    console.log("Hai vinto");
+    console.log(randomUser, randomCpu);
+}
+else if (randomUser < randomCpu){
+    console.log("Hai perso");
+    console.log(randomUser, randomCpu);
+}
+else{
+    console.log("Hai pareggiato");
+    console.log(randomUser, randomCpu);
+}
 
 
 
