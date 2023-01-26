f4ff751deae67baddf7604c0a0a8679898d8ae63
DanieleMarino70 <danielaotitan@gmail.com>
2023-01-26 17:38:42 +0100

    add: remade email + fix: comments

--- js/main.js
+++ js/main.js
@@ -45,42 +45,50 @@ else{
 
 
 
-
-
-
-
-
+// PRENDO L'ELEMENTO DALL'ID "email-button" E LO METTO NELLA CONST buttonEmailEl
+const buttonEmailEl = document.getElementById("email-button");
 // CREO LISTA EMAIL
 const emails = ["lamiaemail@gmail.com", "emailditizio@gmail.com", "emaildicaio@hotmail.it", "email_420diluca@yahoo.com"];
-// CHIEDO ALL'UTENTE LA SUA EMAIL
-const emailRequired = prompt("Qual è la tua email?");
-// CREO UNA VARIABILE SWITCH, IN QUANTO CI INTERESSA SAPERE SE C'E' NELLA LISTA
-let isEmailValid = false;
 
-// debug
-console.log(emails);
+// CREO UN EVENTO AL CLICK DEL BOTTONE
+buttonEmailEl.addEventListener(
+    "click", function(){
+        
+        
+        // PRENDO DAL VALUE DELL'INPUT E METTO DENTRO LA CONST emailRequired
+        const emailRequired = document.getElementById("email-value").value;
+
+        // CREO UNA VARIABILE SWITCH, IN QUANTO CI INTERESSA SAPERE SE C'E' NELLA LISTA
+        let isEmailValid = false;
+        
+        // debug
+        //console.log(emails);
+        
+        
+        // CREO LA CONDIZIONE IN BASE A QUANTI ELEMENTI CI SONO NELLA LISTA
+        for(let i = 0; i < emails.length; i++){
+            // IN "currentEmail" CI RI ITERO IL VALORE CORRENTE DELL'ARRAY EMAIL GRAZIE ALL'INDICE I
+            // ALL'INIZIO DEL FOR SARA' "lamiaemail@gmail.com"
+            const currentEmail = emails[i];
+        
+            //QUA CHIEDO SE L'EMAIL CHE HO RICHIESTO ALL'UTENTE IN PRECEDENZA E' UGUALE A "currentEmail"
+            if(emailRequired == currentEmail){
+                // NEL CASO SIA UGUALE SWITCHO IL FALSE IN TRUE
+                isEmailValid = true;
+            }
+        }
+        
+        //FUORI DAL FOR CHIEDO SE "isEmailValid" E' TRUE,
+        //SE E' TRUE DICO CHE IN PRATICA CHE C'E' NELLA LISTA E QUINDI HA IL PERMESSO
+        if (isEmailValid){
+            alert("Hai il permesso!");
+        }
+        // ALTRIMENTI SE E' FALSE (E QUINDI NON HA SWITCHATO NEL FOR DI PRIMA) NON C'E' NELLA LISTA
+        // E QUINDI NON HA IL PERMESSO       
+        else (alert("Non hai il permesso!"));
+        
+        // TUTTO QUESTO PERO' CONSIDERANDO CHE SIA TUTTO CASESENSITIVE, ALTRIMENTI DOVEVA ESSER FATTO IN UN ALTRO MODO, PENSO :P
 
 
-// CREO LA CONDIZIONE IN BASE A QUANTI ELEMENTI CI SONO NELLA LISTA
-for(let i = 0; i < emails.length; i++){
-    // IN "currentEmail" CI RI ITERO IL VALORE CORRENTE DELL'ARRAY EMAIL GRAZIE ALL'INDICE I
-    // ALL'INIZIO DEL FOR SARA' "lamiaemail@gmail.com"
-    const currentEmail = emails[i];
-
-    //QUA CHIEDO SE L'EMAIL CHE HO RICHIESTO ALL'UTENTE IN PRECEDENZA E' UGUALE A "currentEmail"
-    if(emailRequired == currentEmail){
-        // NEL CASO SIA UGUALE SWITCHO IL FALSE IN TRUE
-        isEmailValid = true;
     }
-}
-
-//FUORI DALL'IF CHIEDO SE "isEmailValid" E' TRUE,
-//SE E' TRUE DICO CHE IN PRATICA CHE C'E' NELLA LISTA E QUINDI HA IL PERMESSO
-if (isEmailValid){
-    alert("Hai il permesso!");
-}
-// ALTRIMENTI SE E' FALSE (E QUINDI NON HA SWITCHATO NEL FOR DI PRIMA) NON C'E' NELLA LISTA
-// E QUINDI NON HA IL PERMESSO       
-else (alert("Non hai il permesso!"));
-
-// TUTTO QUESTO PERO' CONSIDERANDO CHE SIA TUTTO CASESENSITIVE, ALTRIMENTI DOVEVA ESSER FATTO IN UN ALTRO MODO, PENSO :P
+);
